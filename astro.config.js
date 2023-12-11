335eabf45cf3fda67b8a6846ab9037b35b0f6c7f
Levin Keller <post@levinkeller.de>
2023-12-11 07:08:28 +0100

    feat: move to astro

---
+++ astro.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightBlog from 'starlight-blog';
+import react from '@astrojs/react';
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [starlightBlog(), starlight({
+    title: 'My Docs',
+    social: {
+      github: 'https://github.com/withastro/starlight'
+    },
+    components: {
+      MarkdownContent: 'starlight-blog/overrides/MarkdownContent.astro',
+      Sidebar: 'starlight-blog/overrides/Sidebar.astro',
+      ThemeSelect: 'starlight-blog/overrides/ThemeSelect.astro'
+    },
+    sidebar: [{
+      label: 'Guides',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Example Guide',
+        link: '/guides/example/'
+      }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  }), react(), tailwind()]
+});
