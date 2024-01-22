189675cb3b9205b7e7ff06dae4883cac659b6678
Levin Keller <post@levinkeller.de>
2024-01-22 07:55:07 +0100

    fix: linting feat: redirect from old routing

--- shipyard/base/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-import type { Config } from '@/schemas/config'
-interface HeaderProps {
-  config: Pick<Config, 'navbar'>
-}
-
-export const Header: React.FC<HeaderProps> = ({ config }) => <div />
