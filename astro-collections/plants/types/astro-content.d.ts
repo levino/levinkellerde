5a25caa5351ca75486a345ca40e1a85b2c879d99
Levin Keller <post@levinkeller.de>
2025-01-19 07:56:31 +0100

    --wip-- [skip ci]

--- astro-collections/plants/types/astro-content.d.ts
+++
@@ -1,6 +0,0 @@
-import z from 'zod'
-
-declare module 'astro:content' {
-  export function defineCollection(...args: any[]): any;
-  export { z }
-}
---
+++ src/collections/index.ts
@@ -0,0 +1,57 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineCollection, reference, z } from 'astro:content'
+import { vegetablesSchema } from './vegetableSchema'
+import { glob } from 'astro/loaders'
+import { plantSchema } from './plantSchema'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const supplierSchema = z.object({
+  name: z.string(),
+  url: z.string(),
+})
+
+export const collections = {
+  plants: defineCollection({
+    loader: glob({
+      pattern: '*.yaml',
+      base: './content/plants',
+    }),
+    schema: plantSchema,
+  }),
+  suppliers: defineCollection({
+    loader: glob({
+      pattern: '**/*.yaml',
+      base: './content/suppliers',
+    }),
+    schema: supplierSchema,
+  }),
+  plantDescriptions: defineCollection({
+    loader: glob({
+      pattern: '**/*.md',
+      base: './content/plantDescriptions',
+    }),
+    schema: z.object({
+      plant: reference('plants'),
+    }),
+  }),
+  beds: defineCollection({
+    loader: glob({
+      pattern: '**/*.md',
+      base: './content/beds',
+    }),
+    schema: z.object({
+      plants: z.array(reference('plants')),
+      name: z.string(),
+    }),
+  }),
+  vegetables: defineCollection({
+    loader: glob({
+      pattern: '**/*.yaml',
+      base: './content/vegetables',
+    }),
+    schema: vegetablesSchema,
+  }),
+}
